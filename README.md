4adebcccd9f27fc1deecfee77a1c13543d6b29d0
Katrina.Tomaszczyk <katrina.tomaszczyk@entaingroup.com.au>
2025-07-31 09:55:02 +1000

    feat: update README

--- README.md
+++ README.md
@@ -1,13 +1,40 @@
 # vuetify-app
 
+Project is to practice and discover the various Vuetify components by creating a To Do App. 
+
+https://vuetifyjs.com/en/
+
+### To Do App Features
+
+* Menu 
+* Navigation 
+* Lists
+* Routes 
+* Functionality - Add a Task, Complete, Delete 
+
+Practice is from Udemy - Create a Gorgeous Todo App with Vuetify 2, Vue JS 2 & Vuex - using many different Vuetify Components! (Legacy Course)
+
+Instructor: Danny Connell
+
+
 ## Project setup
 ```
-yarn install
+yarn install 
+```
+or
+
+```
+npm install
 ```
 
 ### Compiles and hot-reloads for development
 ```
 yarn serve
+```
+or 
+
+```
+npm run serve
 ```
 
 ### Compiles and minifies for production
@@ -17,3 +44,9 @@ yarn build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Screenshots
+
+<img src="./src/screenshots/ToDoApp.png" width="400" />
+
+<img src="./src/screenshots/ToDoAppMenu.png" width="400" />
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="pa-10">
     <h1>To Do</h1>
+    <v-text-field
+      v-model="newTask"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pl-3 pr-3 mt-5"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      clearable
+    >
+    </v-text-field>
     <v-list two-line flat>
       <v-list-item
         v-for="task in tasks"
@@ -14,8 +25,17 @@
           </v-list-item-action>
 
           <v-list-item-content>
-            <v-list-item-title :class="{ 'text-decoration-line-through' : task.done }">{{ task.title }}</v-list-item-title>
+            <v-list-item-title
+              :class="{ 'text-decoration-line-through': task.done }"
+              >{{ task.title }}</v-list-item-title
+            >
           </v-list-item-content>
+
+          <v-list-item-action>
+            <v-btn icon @click.stop="deleteTask(task.id)">
+              <v-icon color="primary lighten-1">mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
         </template>
       </v-list-item>
     </v-list>
@@ -26,6 +46,7 @@
 export default {
   data() {
     return {
+      newTask: "",
       tasks: [
         {
           id: 1,
@@ -46,11 +67,25 @@ export default {
     };
   },
   methods: {
+    addTask() {
+      if (this.newTask.trim() === "") return;
+
+      const newTask = {
+        id: Date.now(),
+        title: this.newTask,
+        done: false,
+      };
+      this.tasks.push(newTask);
+      this.newTask = "";
+    },
     doneTask(id) {
       const task = this.tasks.find((task) => task.id === id);
       if (task) {
         task.done = !task.done;
       }
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
     },
   },
 };
